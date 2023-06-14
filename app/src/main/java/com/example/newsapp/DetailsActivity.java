8c4cea3d597dfe7989fa450deb106ff5b6475925
FarhanABD <farhanabdilah204@gmail.com>
2023-06-15 05:59:01 +0700

    Commit News App Category

--- app/src/main/java/com/example/newsapp/DetailsActivity.java
+++ app/src/main/java/com/example/newsapp/DetailsActivity.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.newsapp.Models.NewsHeadlines;
+import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends AppCompatActivity {
     NewsHeadlines headlines;
@@ -19,7 +20,19 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
 
         txt_title = findViewById(R.id.text_detail_title);
+        txt_author = findViewById(R.id.text_detail_author);
+        txt_time = findViewById(R.id.text_detail_time);
+        txt_detail = findViewById(R.id.text_detail_detail);
+        txt_content = findViewById(R.id.text_detail_content);
+        img_news = findViewById(R.id.img_detail_news);
 
         headlines = (NewsHeadlines) getIntent().getSerializableExtra("data");
+
+        txt_title.setText(headlines.getTitle());
+        txt_author.setText(headlines.getAuthor());
+        txt_time.setText(headlines.getPublishedAt());
+        txt_detail.setText(headlines.getDescription());
+        txt_content.setText(headlines.getContent());
+        Picasso.get().load(headlines.getUrlToImage()).into(img_news);
     }
 }
